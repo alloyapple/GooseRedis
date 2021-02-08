fc24e332297885a48cf42e7f4d70e5c7589dfbd4
Color <alloy.apple@gmail.com>
2021-02-08 11:41:26 +0800

    初步实现redis

--- Sources/GooseRedis/Connection.swift
+++ Sources/GooseRedis/Connection.swift
@@ -1,4 +1,17 @@
-
+import Foundation
+import Glibc
 public class Connection {
     
+}
+
+public class ConnectionPool {
+    let pid: Int32
+
+    public init() {
+        self.pid = getpid()
+    }
+
+    public func makeConnection() {
+        
+    }
 }
--- Sources/GooseRedis/RedisConfig.swift
+++ Sources/GooseRedis/RedisConfig.swift
@@ -1,4 +1,25 @@
+import Foundation
 
 public struct RedisConfig {
-    
+    let socketTimeout: Float = 0
+    let socketConnectTimeout: Float = 0
+    let socketKeepalive: Bool = true
+    let socketKeepaliveOptions: Int32 = 0
+    let connectionPool: Int32 = 0
+    let unixSocketPath: Int32 = 0
+    let encoding: String = "utf-8"
+    let encodingErrors: String = "strict"
+    let decodeResponses: Int32 = 0
+    let retryOnTimeout: Int32 = 0
+    let ssl: Int32 = 0
+    let sslKeyfile: String = ""
+    let sslCertfile: String = ""
+    let sslCertReqs: Int32 = 0
+    let sslCaCerts: Int32 = 0
+    let sslCheckHostname: Int32 = 0
+    let maxConnections: Int32 = 0
+    let singleConnectionClient: Int32 = 0
+    let healthCheckInterval: Int32 = 0
+    let clientName: String = ""
+    let username: String = ""
 }
