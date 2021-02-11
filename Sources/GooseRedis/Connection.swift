54bfdf1e6828f3935735a6f51d526f594cfdf057
Color <alloy.apple@gmail.com>
2021-02-11 12:48:47 +0800

    添加connect函数

--- Sources/GooseRedis/Connection.swift
+++ Sources/GooseRedis/Connection.swift
@@ -1,7 +1,37 @@
 import Foundation
 import Glibc
+import Goose
+
 public class Connection {
-    
+    public let pid: Int32
+    public let host: String
+    public let port: UInt16
+    public let db: UInt8
+    public let password: String?
+    public var sock: Socket?
+    public let config: RedisConfig
+
+    public init(
+        host: String = "localhost", port: UInt16 = 6379,
+        db: UInt8 = 0, password: String? = nil, config: RedisConfig = defaultConfig
+    ) {
+        self.pid = getpid()
+        self.host = host
+        self.port = port
+        self.db = db
+        self.password = password
+        self.config = config
+    }
+
+    public func connect() {
+        guard let sock = self.sock else {
+            return
+        }
+    }
+
+    public func _connect() {
+        
+    }
 }
 
 public class ConnectionPool {
@@ -12,6 +42,6 @@ public class ConnectionPool {
     }
 
     public func makeConnection() {
-        
+
     }
 }
